6583becaabe0e8e895ad7f5322eb17798696aebe
sudeep phuyel <sudipphuyel41@gmail.com>
2021-02-06 15:55:33 +0545

    gallery

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Photo Gallery</div>
+
+                <div class="card-body">
+                @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                   
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card">
+                    <div class="card-header">Create New Gallery</div>
+                    <div class="card-body">
+                    <a href="{{route('creategallery)}}" class="btn btn-success btn-block">Create New Gallery</a>
+                    </div>
+            </div>
+            </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+        <div class="jumbotron">
+          <h1>Welcome to the Photo Gallery</h1>
+          <p>You need to Register before using this photo gallery and Log In using your Credentials to either view or upload your Photos.</p>
+        </div>
+@endsection
